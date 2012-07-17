7f9d4d6ff47a216a1170224acbb6a9f5ae194c6f
Thasso Griebel <thasso.griebel@googlemail.com>
2012-07-17 14:53:15 +0200

    automiatic detecty of objects

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,24 +1,26 @@
+import os
 from ez_setup import use_setuptools
 
 use_setuptools()
 from setuptools import setup
 from distutils.core import Extension
 
+gemtools_dir = "../../GEMTools/"
+objs = []
+for f in os.listdir(gemtools_dir):
+    if f.endswith(".o"):
+        objs.append(gemtools_dir+f)
+
+
 
 gemtools = Extension('gem.gemtools',
                     define_macros=[('MAJOR_VERSION', '1'),
                                    ('MINOR_VERSION', '0')],
                     include_dirs=['../../GEMTools'],
-                    extra_objects=['../../GEMTools/gem_tools.o', '../../GEMTools/gt_error.o',
-                                   '../../GEMTools/gt_output_map.o', '../../GEMTools/gt_alignment.o',
-                                   '../../GEMTools/gt_input_file.o', '../../GEMTools/gt_template.o',
-                                   '../../GEMTools/gt_buffered_map_input.o', '../../GEMTools/gt_map.o',
-                                   '../../GEMTools/gt_vector.o', '../../GEMTools/gt_commons.o',
-                                   '../../GEMTools/gt_misms.o'],
+                    extra_objects=objs,
                     sources=['gem/py_iterator.c', 'gem/py_template_iterator.c',
                                'gem/py_mismatch.c', 'gem/py_map.c', 'gem/py_alignment.c',
                                'gem/py_template.c', 'gem/gemtoolsmodule.c', 'gem/py_mappings_iterator.c'])
-
 
 setup(
         name='Gem',
