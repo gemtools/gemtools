9efb8377ef82406e11d09871524be649096750e0
Thasso Griebel <thasso.griebel@gmail.com>
2012-09-27 17:33:04 +0200

    fixed typo to set debug output stream properly

--- bindings/python/gem/__init__.py
+++ bindings/python/gem/__init__.py
@@ -69,7 +69,7 @@ def loglevel(level):
         raise ValueError('Invalid log level: %s' % loglevel)
     logging.basicConfig(level=numeric_level)
     logging.getLogger().setLevel(numeric_level)
-    if level.upper() in ["info"]:
+    if level.upper() in ["debug"]:
         log_output = LOG_STDERR
 
 
