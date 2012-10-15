516e2de32133d9c31d88d177ab22fcb108af2ddb
Thasso Griebel <thasso.griebel@googlemail.com>
2012-10-15 14:13:07 +0200

    Bumped to version 1.3

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -12,18 +12,16 @@ for f in os.listdir(gemtools_dir):
 
 gemtools = Extension('gem.gemtools',
                     define_macros=[('MAJOR_VERSION', '1'),
-                                   ('MINOR_VERSION', '1')],
+                                   ('MINOR_VERSION', '3')],
                     include_dirs=['../../GEMTools'],
                     extra_objects=objs,
                     sources=['src/py_iterator.c', 'src/py_template_iterator.c',
                                'src/py_mismatch.c', 'src/py_map.c', 'src/py_alignment.c',
                                'src/py_template.c', 'src/gemtoolsmodule.c', 'src/py_mappings_iterator.c'])
 
-#gem_binaries = "../../binaries/gem-2.2"
-
 setup(
         name='Gem',
-        version='1.2',
+        version='1.3',
         description='Python support library for the GEM mapper and gemtools',
         author='Thasso Griebel',
         author_email='thasso.griebel@gmail.com',
