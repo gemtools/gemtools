92125898c7109d9f3c305b7614b39fe19bba87be
Thasso Griebel <thasso.griebel@googlemail.com>
2012-10-17 15:50:18 +0200

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -2,47 +2,65 @@ import os
 from setuptools import setup
 from distutils.core import Extension
 from setuptools.command.install import install as _install
+from setuptools.command.build_ext import build_ext as _build_ext
 import sys
+import subprocess
 
+# hack the setup tools installation
+# to make sure bundled binaries are 
+# executable after install
 class install(_install):
     def run(self):
         _install.run(self)
-        bins=[x for x in os.listdir("gem/gembinaries")]
+        bins=[x for x in os.listdir("python/gem/gembinaries")]
         for file in self.get_outputs():
             if os.path.basename(file) in bins:
                 print "Making %s executable" % file
                 os.chmod(file, 0755)
 
-gemtools_dir = "../../GEMTools/"
-objs = []
-for f in os.listdir(gemtools_dir):
-    if f.endswith(".o"):
-        objs.append(gemtools_dir+f)
 
+class build_ext(_build_ext):
+    def run(self):
+        process = subprocess.Popen(['make'], shell=True, cwd='GEMTools')
+        if process.wait() != 0:
+            raise ValueError("Error while compiling GEMTools")
+        _build_ext.run(self)
 
 
 gemtools = Extension('gem.gemtools',
                     define_macros=[('MAJOR_VERSION', '1'),
                                    ('MINOR_VERSION', '3')],
-                    include_dirs=['../../GEMTools'],
-                    extra_objects=objs,
-                    sources=['src/py_iterator.c', 'src/py_template_iterator.c',
-                               'src/py_mismatch.c', 'src/py_map.c', 'src/py_alignment.c',
-                               'src/py_template.c', 'src/gemtoolsmodule.c', 'src/py_mappings_iterator.c'])
+                    include_dirs=['GEMTools/include', 'GEMTools/resources/include/'],
+                    library_dirs=['GEMTools/lib'],
+                    libraries=['gemtools'],
+                    sources=['python/src/py_iterator.c', 'python/src/py_template_iterator.c',
+                               'python/src/py_mismatch.c', 'python/src/py_map.c', 'python/src/py_alignment.c',
+                               'python/src/py_template.c', 'python/src/gemtoolsmodule.c', 'python/src/py_mappings_iterator.c'])
 
 setup(
-        cmdclass={'install': install},
+        cmdclass={'install': install, 'build_ext': build_ext},
         name='Gem',
         version='1.3',
-        description='Python support library for the GEM mapper and gemtools',
+        description='Python support library for the GEM mapper and the gemtools library',
         author='Thasso Griebel',
         author_email='thasso.griebel@gmail.com',
-        url='http://barnaserver.com/gemtools',
-        long_description='''
-        This is the python binding to the gemtools library.
-        ''',
+        url='http://algorithms.cnag.cat/',
+        long_description='''This is the python binding and wrapper library around the GEM mapper.
+The module allows you to run teh GEM mapper and simplifies building mapping
+pipeline in python. In addition, we provide a fast C based parsing library that
+is used to parse GEM results and extract mapping information.
+
+For more information about the GEM see
+
+http://algorithms.cnag.cat/
+
+The code for this project can be found on github:
+
+
+''',
+        package_dir={'':'python'},
         packages=['gem'],
-        package_data={"": ["%s/%s" % ("gem/gembinaries",x) for x in os.listdir("gem/gembinaries")]},
+        package_data={"": ["%s/%s" % ("gem/gembinaries",x) for x in os.listdir("python/gem/gembinaries")]},
         ext_modules=[gemtools],
         test_suite='nose.collector',
         zip_safe=False,
