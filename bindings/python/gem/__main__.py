ff4f5bd2f81bda32d346f5f319fe9e3ead46519e
Thasso Griebel <thasso.griebel@googlemail.com>
2012-10-17 12:28:22 +0200

    Moved python directory and removed old scripts and examples

--- bindings/python/gem/__main__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import gem
-
-__author__ = 'Thasso Griebel <thasso.griebel@gmail.com>'
-if __name__ == "__main__":
-    gem.validate_executables()
--- bindings/python/test/gemtools_filehandles_tests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import gem.gemtools as gt
-from testfiles import testfiles
-
-
-def test_open_map_file():
-    infile = gt.open_file(testfiles["bedconvert.map"])
-    assert infile != None
