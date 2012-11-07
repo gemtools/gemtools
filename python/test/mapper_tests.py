178ee5a1df02742e8d802cb1b7d6555482bfd83e
Thasso Griebel <thasso.griebel@gmail.com>
2012-11-07 15:11:23 +0100

    indexer test, more messages

--- python/test/mapper_tests.py
+++ python/test/mapper_tests.py
@@ -61,7 +61,7 @@ def test_indexer_output_suffix():
     result = results_dir + "/genome_index.gem"
     input = testfiles["genome.fa"]
     index = gem.index(input, result)
-    assert index == result+".gem"
+    assert index == result+".gem", "Result should be %s.gem but is %s" % (result, index)
     assert os.path.exists(result+".gem")
 
 
