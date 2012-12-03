eff1c6db650ad4f6f4211698bcdf519c3a5e10ca
Thasso Griebel <thasso.griebel@googlemail.com>
2012-12-03 11:03:03 +0100

    Haning gt_template_merge test

--- GEMTools/test/gt_suite_template_utils.c
+++ GEMTools/test/gt_suite_template_utils.c
@@ -37,6 +37,23 @@ START_TEST(gt_test_template_merge)
 }
 END_TEST
 
+START_TEST(gt_test_template_merge_hang)
+{
+
+  fail_unless(gt_input_map_parse_template(
+      "HWI-962:71:D0PEYACXX:4:1101:18640:2354/2\tCGCGCGGGAGCCAGCAGGAGCACCAGCTGCGCAGGCAGGTTGAACTGCTGGCTTATAAAGTAGAGCAGGAGAAGT\t@CCFFFFFHHHHHIJIJJIJJJJJJJIIJGIDGIHHHFF6>CCCDEDDDDCBDD>CCDCC>CCCCCDDDDDBDC>\t0:0:0:0:0:0\t-",source)==0);
+  fail_unless(gt_input_map_parse_template(
+      "HWI-962:71:D0PEYACXX:4:1101:18640:2354/2\tCGCGCGGGAGCCAGCAGGAGCACCAGCTGCGCAGGCAGGTTGAACTGCTGGCTTATAAAGTAGAGCAGGAGAAGT\t@CCFFFFFHHHHHIJIJJIJJJJJJJIIJGIDGIHHHFF6>CCCDEDDDDCBDD>CCDCC>CCCCCDDDDDBDC>\t0:1\tchr21:+:47848466:39>1419*36",target)==0);
+  // merge into source
+  gt_template_merge_template_mmaps(source,target);
+  gt_string* string = gt_string_new(1024);
+  gt_output_map_sprint_template(string, source, GT_ALL, true);
+  // convert to string
+  char * line = gt_string_get_string(string);
+  fail_unless(gt_streq(line,"HWI-962:71:D0PEYACXX:4:1101:18640:2354/2\tCGCGCGGGAGCCAGCAGGAGCACCAGCTGCGCAGGCAGGTTGAACTGCTGGCTTATAAAGTAGAGCAGGAGAAGT\t@CCFFFFFHHHHHIJIJJIJJJJJJJIIJGIDGIHHHFF6>CCCDEDDDDCBDD>CCDCC>CCCCCDDDDDBDC>\t0:1\tchr21:+:47848466:39>1419*36\n"));
+}
+END_TEST
+
 START_TEST(gt_test_template_to_string)
 {
 
@@ -108,6 +125,7 @@ Suite *gt_template_utils_suite(void) {
   TCase *test_case = tcase_create("Template Utils");
   tcase_add_checked_fixture(test_case,gt_template_utils_setup,gt_template_utils_teardown);
   tcase_add_test(test_case,gt_test_template_merge);
+  tcase_add_test(test_case,gt_test_template_merge_hang);
   tcase_add_test(test_case,gt_test_template_to_string);
   tcase_add_test(test_case,gt_test_template_copy);
   suite_add_tcase(s,test_case);
