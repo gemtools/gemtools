93d24b8d85d735cfcbdd370cfb26e6180f4e15a4
Thasso Griebel <thasso.griebel@googlemail.com>
2013-06-25 11:16:17 +0200

    Bumped version to 1.6.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ import shutil
 
 
 __VERSION_MAJOR = "1"
-__VERSION_MINOR = "6.1"
+__VERSION_MINOR = "6.2"
 __VERSION__ = "%s.%s" % (__VERSION_MAJOR, __VERSION_MINOR)
 __DOWNLOAD_FILE_TEMPLATE__ = "GEM-gemtools-%s-%s.tar.gz"
 __DOWNLOAD_URL__ = "http://barnaserver.com/gemtools/"
